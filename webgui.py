60eceedbeb5910941f61636012d79739c8694bc6
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-12-02 02:06:05 +0100

    Saving and reloading image

--- webgui.py
+++ webgui.py
@@ -1,7 +1,10 @@
 import socket
 
+import flask
 from flask import Flask, request, render_template
 import random, threading, webbrowser
+
+from graphviz import Digraph, Source
 
 app = Flask(
     __name__,
@@ -11,23 +14,54 @@ app = Flask(
 )
 
 
-@app.route("/")
+def render_image(text):
+    graph = Source(text)
+    graph.save('data/graph')
+    graph.render('static/img/graph', format='png')
+
+
+def read_text_from_file():
+    graph = Source.from_file('data/graph')
+    return graph.source.strip()
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    text = request.form.get('text', read_text_from_file())
+    context = {'text': text, }
+    render_image(text)
+    rendered_template = render_template("index.html", **context)
+    response = flask.make_response(rendered_template)
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    return response
 
 
-def get_free_tcp_port():
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.bind(('', 0))
-    _, port = tcp.getsockname()
-    hostname = socket.gethostname()
-    ip_addr = socket.gethostbyname(hostname)
-    tcp.close()
-    # return ip_addr, port
-    return '192.168.178.56', 5555
+@app.after_request
+def add_header(request):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    request.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    request.headers["Pragma"] = "no-cache"
+    request.headers["Expires"] = "0"
+    request.headers['Cache-Control'] = 'public, max-age=0'
+    return request
 
 
 if __name__ == "__main__":
+    def get_free_tcp_port():
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind(('', 0))
+        _, port = tcp.getsockname()
+        hostname = socket.gethostname()
+        ip_addr = socket.gethostbyname(hostname)
+        tcp.close()
+        # return ip_addr, port
+        # FIXME: Reactivate ip address and port
+        return '192.168.178.56', 5555
+
+
     addr, port = get_free_tcp_port()
     url = 'http://{}:{}/'.format(addr, port)
     # threading.Timer(1.25, lambda: webbrowser.open_new(url)).start()
